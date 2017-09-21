fa831a6bb72d41d0b508cfc064a1a2181abc5f37
hamedasemi <h-@live.se>
2017-09-21 21:49:02 +0200

    Add rouutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -8,6 +9,20 @@ import { ContactComponent } from './contact/contact.component';
 import { NarrativeComponent } from './narrative/narrative.component';
 import { CommercialComponent } from './commercial/commercial.component';
 import { PhotographyComponent } from './photography/photography.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { VideosComponent } from './videos/videos.component';
+import { PhotosComponent } from './photos/photos.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent, pathMatch: 'full' },
+  { path: 'commercial', component: CommercialComponent },
+  { path: 'narrative', component: NarrativeComponent },
+  { path: 'photography', component: PhotographyComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,10 +32,19 @@ import { PhotographyComponent } from './photography/photography.component';
     ContactComponent,
     NarrativeComponent,
     CommercialComponent,
-    PhotographyComponent
+    PhotographyComponent,
+    PageNotFoundComponent,
+    JumbotronComponent,
+    VideosComponent,
+    PhotosComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    // other imports here
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
